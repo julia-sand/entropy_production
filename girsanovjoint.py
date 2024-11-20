d34ff23e35e3dfd2ce0a793b4b0df7c33f721f76
julia-sand <73934921+julia-sand@users.noreply.github.com>
2024-11-20 16:03:56 +0200

    Update girsanovjoint.py

--- girsanovjoint.py
+++ girsanovjoint.py
@@ -108,7 +108,7 @@ fig_joint_distributions_meshgrid = plt.figure(figsize=(15,10))
 gs_joint_distributions = fig_joint_distributions_meshgrid.add_gridspec(2, 3, width_ratios=[1, 1, 1], height_ratios=[1, 1])
 
 for k in enumerate(plot_times):
-  plots.joint_distributions_scatter(fig_joint_distributions_meshgrid, 5-k[0],
+  plots.joint_distributions_scatter(fig_joint_distributions_meshgrid,gs_joint_distributions, 5-k[0],
                               df_girspdf_ep[df_girspdf_ep["t"] == k[1]].ptx.to_numpy(),
                               Q,P,
                               k[1],vmax)
