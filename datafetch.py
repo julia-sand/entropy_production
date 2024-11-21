26d58e563f83f87919f70e44c01c412d9ae3e4c7
julia-sand <73934921+julia-sand@users.noreply.github.com>
2024-11-21 12:54:18 +0200

    Update datafetch.py

--- datafetch.py
+++ datafetch.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("results.csv",sep = " ", header = 1)
+df = pd.read_csv("results.csv",sep = " ", header = 0)
 
 #fill infs with zeros
 #df.replace([np.inf,-np.inf,np.nan], 0, inplace=True)
