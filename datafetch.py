c0063e86864eda48222de6aec926ad61509c6f9c
julia-sand <73934921+julia-sand@users.noreply.github.com>
2024-11-21 13:11:50 +0200

    Update datafetch.py

--- datafetch.py
+++ datafetch.py
@@ -1,6 +1,10 @@
 import pandas as pd
+from main import *
 
 df = pd.read_csv("results.csv",sep = " ", header = 0)
+
+#add t0 column
+df.t0 = np.round((epsilon**2)*df.t ,dps)
 
 #fill infs with zeros
 #df.replace([np.inf,-np.inf,np.nan], 0, inplace=True)
