506ac30a5f2c7e56be625b73a984edb4a6fcc8f4
julia-sand <73934921+julia-sand@users.noreply.github.com>
2024-11-26 11:42:17 +0200

    Update plotdrift.py

--- plotdrift.py
+++ plotdrift.py
@@ -16,7 +16,6 @@ titlepad = 5
 titlex = 0.15
 titley = 0.85
 titlezorder = 1000
-titles = ["(a)","(b)","(c)","(d)","(e)","(f)","(g)","(h)","(i)","(j)","(k)","(l)","(m)","(n)","(o)","(p)","(q)"]
 
 #dist title location
 disttitlex = -2.8
