af49574b3bd0570ab7ddff1533bed006d5ed10f2
Julia Sanders <julisand@puhti-login11.bullx>
2024-12-03 15:05:55 +0200

    modified:   histograms.py

--- histograms.py
+++ histograms.py
@@ -124,5 +124,5 @@ fig_distributions.tight_layout(rect=(0, bbox.y1, 1, 1), h_pad=0.5, w_pad=0.5)
 df_ep_cumulants_exp.to_csv("cumulants.csv")
 
 #save the histogram
-plt.savefig("histograms_test.pdf",bbox_to_inches="tight")
+plt.savefig("histograms_test.pdf",bbox_inches="tight")
 
