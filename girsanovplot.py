caa0c90fc94a2d251e203f2ce64692d279a70ce1
julia-sand <73934921+julia-sand@users.noreply.github.com>
2024-12-05 11:36:20 +0200

    Update girsanovplot.py

--- girsanovplot.py
+++ girsanovplot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+from matplotlib.cm import ScalarMappable
 
 from main import *
 from datafetch import *
@@ -38,5 +39,35 @@ fig_joint_distributions_meshgrid.subplots_adjust(
      wspace=0.85,# The width of the padding between subplots, as a fraction of the average Axes width.
     hspace=0.65# The height of the padding between subplots, as a fraction of the average Axes height.
 )
+
+#add the legend
+sm =  ScalarMappable(norm="log",cmap=plt.get_cmap("Blues"))
+sm.set_array([])
+sm.set_clim(vmin=0.001, vmax=vmax)
+
+cbar_ax = fig_joint_distributions_meshgrid.add_axes([0.2, -0.1, 0.75, 0.05])
+cb = fig_joint_distributions_meshgrid.colorbar(sm,
+                                          cax=cbar_ax,
+                                          orientation="horizontal")
+
+cb.ax.tick_params(labelsize=fontsizeticks)
+cb.ax.set_xlabel("Joint Distribution",fontsize=fontsizetitles)
+
+#get legend
+blue_line = mlines.Line2D([], [],color=c1,lw=lw)
+orange_line = mlines.Line2D([], [],color="orange",lw=lw)
+green_line = mlines.Line2D([], [],color="green",lw=lw,linestyle="dashed")
+
+legend = fig_joint_distributions_meshgrid.legend(handles=[blue_line,orange_line,green_line],
+          labels = ["Monte Carlo w. Girsanov","Perturbative Estimates","Gaussian Prediction"],
+          fontsize = fontsizeticks,
+          frameon = False,
+          handlelength = 1,
+          ncols = 2,
+          bbox_to_anchor=(0.75,0.2))
+
+#move the legend
+legend.set_bbox_to_anchor(bbox=(0.75,0.2))
+
 
 plt.savefig("test.pdf",bbox_inches="tight")
